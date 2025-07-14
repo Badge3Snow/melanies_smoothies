42873e8e1682f7be1b1ef086aff535866e281544
Badge3Snow <tracydower@accentcare.com>
2025-07-13 21:10:59 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,20 +1,14 @@
 # Import python packages
 import streamlit as st
-# from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
+import requests
 
-# Write directly to the app
-# {st.__version__}
 st.title(f":cup_with_straw: Customize your Smooothie :cup_with_straw: ")
 st.write("""Choose the fruits you want in your smoothie!""")
 
-
 cnx = st.connection("snowflake")
 session = cnx.session()
-# session = get_active_session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-# st.dataframe(data=my_dataframe, use_container_width=True)
-
 
 ingredients_list = st.multiselect(
     "Choose up to 5 ingredients:"
@@ -27,23 +21,20 @@ st.write("The name on your Smoothie will be:", name_on_order)
 
 if ingredients_list: # if ingredients_list is not null: then do everything below this line that is indented. 
     st.write("You selected:", ingredients_list) # Numbered, vertical
-
     ingredients_string =''
-
-    for each_fruit in ingredients_list:
+    for fruit_chosen in ingredients_list:
         ingredients_string += each_fruit + ' '
-    
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+        sf_df = st.dataframe(smoothiefroot_response.json(),use_container_width=true)
+
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
                 values ('""" + ingredients_string + """','""" + name_on_order + """')"""
-
     time_to_insert = st.button('Submit Order')
     
-
     if time_to_insert:
-        session.sql(my_insert_stmt).collect()
-        
+        session.sql(my_insert_stmt).collect()        
         st.success((name_on_order, ' your Smoothie is ordered!'), icon="✅")
-import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json())
+
+
+
 
