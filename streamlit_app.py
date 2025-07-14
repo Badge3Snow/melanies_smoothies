c752f94b13b57f9f12a96a365d4c197064405b60
Badge3Snow <tracydower@accentcare.com>
2025-07-13 22:44:30 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,9 @@ cnx = st.connection("snowflake")
 session = cnx.session()
 session.sql("use warehouse COMPUTE_WH").collect()     # new as of 10 pm
 
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
+st.dataframe(data=my_dataframe,use_container_width=True)
+st.stop()
 
 ingredients_list = st.multiselect(
     "Choose up to 5 ingredients:"
